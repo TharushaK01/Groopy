7c618e18f1fe58226d6f9e33602f6bb7bba119a5
TKA Products <tharushakavinda01@gmail.com>
2025-02-25 01:08:29 +0530

    Trip done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ import { DrinksComponent } from './drinks/drinks.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { PhotographersComponent } from './photographers/photographers.component';
 import { DecorationComponent } from './decoration/decoration.component';
+import { LocationComponent } from './location/location.component';
+import { GuideComponent } from './guide/guide.component';
+import { AirticketsComponent } from './airtickets/airtickets.component';
+import { VenuesComponent } from './venues/venues.component';
 
 
 
@@ -26,7 +30,10 @@ const routes: Routes = [
   { path: 'vehicles', component: VehiclesComponent},
   { path: 'photographers', component: PhotographersComponent},
   { path: 'decoration', component: DecorationComponent},
-  
+  { path: 'location', component: LocationComponent},
+  { path: 'guide', component: GuideComponent},
+  { path: 'airtickets', component: AirticketsComponent},
+  { path: 'venues', component: VenuesComponent},
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,10 @@ import { DrinksComponent } from './drinks/drinks.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { PhotographersComponent } from './photographers/photographers.component';
 import { DecorationComponent } from './decoration/decoration.component';
+import { LocationComponent } from './location/location.component';
+import { GuideComponent } from './guide/guide.component';
+import { AirticketsComponent } from './airtickets/airtickets.component';
+import { VenuesComponent } from './venues/venues.component';
 // import { ChatComponent } from './chat/chat.component'; 
 
 
@@ -47,6 +51,10 @@ import { DecorationComponent } from './decoration/decoration.component';
     VehiclesComponent,
     PhotographersComponent,
     DecorationComponent,
+    LocationComponent,
+    GuideComponent,
+    AirticketsComponent,
+    VenuesComponent,
     // ChatComponent
   ],
   imports: [
