5c7ea01525d7d7d8d14456bbb71c6f1b25030598
TKA Products <tharushakavinda01@gmail.com>
2025-02-26 18:00:02 +0530

    3 categories done

---
+++ src/app/sound/sound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sound',
+  templateUrl: './sound.component.html',
+  styleUrl: './sound.component.scss'
+})
+export class SoundComponent {
+
+}
