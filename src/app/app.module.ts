7ef091f3a55d7572a0df9df81b61c01442ba3340
TKA Products <tharushakavinda01@gmail.com>
2025-04-11 01:56:25 +0530

    error fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { ProfileComponent } from './profile/profile.component';
-
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 
 
@@ -88,15 +88,22 @@ import { ProfileComponent } from './profile/profile.component';
     FormsModule,
     ReactiveFormsModule,
     MatAutocompleteModule,
-    MatMenuModule,
+
  
 
    
   ],
   providers: [
     provideAnimationsAsync(),
-    provideFirebaseApp(() => initializeApp({"projectId":"groopy-db888","appId":"1:121507118517:web:6bfac9ff72bb46a92eadfc","storageBucket":"groopy-db888.firebasestorage.app","apiKey":"AIzaSyDPBbJbwKy5Oo0iWV-SrYeDCqNaBB0ckfo","authDomain":"groopy-db888.firebaseapp.com","messagingSenderId":"121507118517"})),
-    provideAuth(() => getAuth())
+    provideFirebaseApp(() => initializeApp({
+      "projectId":"groopy-db888",
+      "appId":"1:121507118517:web:6bfac9ff72bb46a92eadfc",
+      "storageBucket":"groopy-db888.firebasestorage.app",
+      "apiKey":"AIzaSyDPBbJbwKy5Oo0iWV-SrYeDCqNaBB0ckfo",
+      "authDomain":"groopy-db888.firebaseapp.com",
+      "messagingSenderId":"121507118517"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(()=> getFirestore()),
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { doc, docData, Firestore, setDoc, updateDoc } from '@angular/fire/firestore';
+import { from, Observable, of, switchMap } from 'rxjs';
+import { ProfileUser } from '../models/user-profile';
+import { AuthenticationService } from './authentication.service';
+import { User } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  get currentUserProfile$(): Observable<ProfileUser | null> {
+    return this.authService.currentUser$.pipe(
+      switchMap((user: User | null) => {
+        
+        if (!user?.uid) {
+          return of(null);
+        }
+        const ref = doc(this.firestore, 'users', user?.uid);
+        return docData(ref) as Observable<ProfileUser>;
+      })
+    )
+  }
+
+  constructor(private firestore: Firestore, private authService: AuthenticationService) { }
+
+  addUser(user: ProfileUser) : Observable<any>  {
+    const ref = doc(this.firestore, 'users', user?.uid);
+    return from(setDoc(ref,user));
+  }
+
+  updateUser(user: ProfileUser) : Observable<any>  {
+    const ref = doc(this.firestore, 'users', user?.uid);
+    return from(updateDoc(ref,{...user}));
+  }
 }
